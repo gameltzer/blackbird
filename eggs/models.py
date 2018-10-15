44a8aa1a19165b70b157a37b2b1c887a079e1397
gameltzer <gameltzer@gmail.com>
2018-10-15 11:58:19 -0500

    second commit. This should contain the entire contents of the eggs app

---
+++ eggs/models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.core.files import File
+
+
+# Create your models here.
+
+class Reference(models.Model):
+# This is our primary key which auto-increments
+
+    referenceID = models.AutoField(primary_key=True)
+    referenceFile = models.FileField(upload_to="reference/")  
+    def __str__(self):
+        return self.referenceFile.name
+
+
+
+#This is for each sample
+class Sample(models.Model):
+    #This refers to the reference file associated with each genetic sample
+    reference = models.ForeignKey(Reference, on_delete=models.DO_NOTHING)
+    #This is a char field because there is no reason why the submission name needs to be super long.
+    #   The first argument to CharField is intended as a human readable name.
+    batch = models.CharField( max_length=100)
+    sampleFile = models.FileField(upload_to="sample") 
+    def __str__(self):
+        return self.sampleFile.name 
+    
+
+
+
---
+++ eggs/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^upload$',views.upload, name ='upload'),
+    url(r'^upload/uploadSample$',views.uploadSample, name ='uploadSample'),
+    url(r'^submit$',views.submit, name ='submit'),
+    url(r'^graph$',views.graph, name='graph'),
+]
