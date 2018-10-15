86b8d8459e84298c7eb234041e38a0d42c243898
gameltzer <gameltzer@gmail.com>
2018-10-15 12:45:13 -0500

    this is an attempt to incorporate some of the site level files

---
+++ blackbird/urls.py
@@ -0,0 +1,25 @@
+"""blackbird URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+# I am done testing things, so I comment the, worms reference out!     
+#    url(r'worms/', include('worms.urls')),
+    url(r'eggs/', include('eggs.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+
