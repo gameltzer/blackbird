3bce7a652f5621ce80898e6e5d7ebdc1245267a5
Gabriel Meltzer <gameltzer@uchicago.edu>
2018-11-05 15:16:31 -0600

    well

--- blackbird/settings.py
+++ blackbird/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 CSRF_COOKIE_SECURE=True
 
-SESSION_COOKIE_SECURE=False
+SESSION_COOKIE_SECURE=True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
