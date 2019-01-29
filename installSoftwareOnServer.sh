92de32b5020901f032d01272bba4f015b70e0577
Gabriel Meltzer <gameltzer@uchicago.edu>
2019-01-29 15:36:29 -0600

    adding software install script

--- installSoftwareOnServer.sh
+++ installSoftwareOnServer.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 # This installs additional linux software packages necessary for our application.
-
-cd /opt/python/current/app
+:wcd /opt/python/current/app
 cd bcftools-1.9
-sudo ./configure /usr/local
+sudo ./configure prefix=/usr/local
 sudo make
 sudo make install
-
 cd ../samtools-1.9
-sudo ./configure /usr/locals
+sudo ./configure prefix=/usr/local
 sudo make
 sudo make install
-
 cd ../bowtie2-2.3.4.3
 sudo make static-lib
 sudo make STATIC_BUILD=1
