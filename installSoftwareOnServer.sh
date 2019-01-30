05a8725d578720639e08b2741a27284fefc4cc17
Gabriel Meltzer <gameltzer@uchicago.edu>
2019-01-30 12:08:02 -0600

    trying to reduce memory

--- installSoftwareOnServer.sh
+++ installSoftwareOnServer.sh
@@ -2,14 +2,6 @@
 # This installs additional linux software packages necessary for our application.
 cd /opt/python/current/app
 cd bcftools-1.9
-sudo ./configure prefix=/usr/local
-sudo make
-sudo make install
-cd ../samtools-1.9
-sudo ./configure prefix=/usr/local
-sudo make
-sudo make install
+sudo ./configure prefix=/usr/local && sudo make && sudo make install && cd ../samtools-1.9 && sudo ./configure prefix=/usr/local && sudo make && sudo make install
 cd ../bowtie2-2.3.4.3
-sudo make static-lib
-sudo make STATIC_BUILD=1
-sudo make sudo make install
+sudo make static-libs && sudo make STATIC_BUILD=1 && sudo make && sudo make install
